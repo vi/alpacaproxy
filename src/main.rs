97631950e9ff408704721cf893c1a8fabfecc548
Vitaly _Vi Shukela <vi0oss@gmail.com>
2021-09-16 00:13:24 +0300

    Implement prerolling

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{net::SocketAddr, path::PathBuf};
 
 use std::time::Duration;
 use anyhow::Context;
+use futures::stream::SplitSink;
 use futures::{sink::SinkExt,stream::StreamExt};
 
 use tokio_tungstenite::tungstenite::Message as WebsocketMessage;
@@ -23,7 +24,18 @@ struct Opts {
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 struct Message {
     stream: String,
-    data: serde_json::Map<String, serde_json::Value>,
+    data: serde_json::Value,
+}
+
+#[derive(serde_derive::Serialize, serde_derive::Deserialize)]
+struct MinutelyData {
+    ev: String,
+
+    #[serde(rename="T")]
+    t: String,
+
+    #[serde(flatten)]
+    rest: serde_json::Value,
 }
 
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
@@ -32,15 +44,95 @@ struct ClientConfig {
     uri: http::Uri,
 
     startup_messages: Vec<serde_json::Value>,
-
-    preroll_messages: u64,
 }
 
-async fn serve_client(mut client_socket: tokio::net::TcpStream) -> anyhow::Result<()> {
-    let mut client = tokio_tungstenite::accept_async(client_socket).await?;
+fn get_next_db_key(db: &sled::Db) -> anyhow::Result<u64> {
+    let nextkey = match db.last()? {
+        None => 0,
+        Some((k, _v)) => {
+            let k : [u8; 8] = k.as_ref().try_into()?;
+            u64::from_be_bytes(k).checked_add(1).context("Key space is full")?
+        }
+    };
+    Ok(nextkey)
+}
+
+async fn serve_client(mut client_socket: tokio::net::TcpStream, db: &sled::Db) -> anyhow::Result<()> {
+    let (mut client_sink, mut client_stream) = tokio_tungstenite::accept_async(client_socket).await?.split();
     {
-        let mut buf = serde_json::to_string(&Message{stream:"hello".to_owned(), data:serde_json::Map::new()}).unwrap();
-        client.send(WebsocketMessage::Text(buf)).await?;
+        let mut buf = serde_json::to_string(&Message{stream:"hello".to_owned(), data: serde_json::Value::Null}).unwrap();
+        client_sink.send(WebsocketMessage::Text(buf)).await?;
+    }
+    let mut cursor = get_next_db_key(db)?;
+ 
+    let err_to_client = |mut client_sink: SplitSink<_,_>, x:String| {
+        log::warn!("Sending error to client: {}", x);
+        async move {
+            client_sink.send(WebsocketMessage::Text(serde_json::to_string(&Message{stream:"error".to_owned(), data:serde_json::Value::String(x)}).unwrap())).await?;
+            let ret : anyhow::Result<_> = Ok(client_sink);
+            ret
+        }  
+    };
+
+    let handle_msg = |msg: Message, mut client_sink:  SplitSink<_,_>| -> _ {
+        async move {
+            match &msg.stream[..] {
+                "preroll" => {
+                    let num = match msg.data {
+                        serde_json::Value::Number(x) => x.as_u64().context("Cannot handle nubmer of preroll messages")?,
+                        _ => {
+                            client_sink = err_to_client(client_sink, "Invalid type of `data` for `preroll` message, expected a number".to_owned()).await?;
+                            return Ok(client_sink)
+                        }
+                    };
+                    log::info!("  prerolling {} messages for the client", num);
+                    let start = cursor.saturating_sub(num);
+                    for x in start..cursor {
+                        match db.get(x.to_be_bytes())? {
+                            Some(v) => {
+                                let minutely : MinutelyData = match serde_json::from_slice(&v) {
+                                    Ok(x) => x,
+                                    Err(_e) => {
+                                        log::warn!("Failed to properly deserialize minutely datum number {}", x);
+                                        continue
+                                    }
+                                };
+                                let stream = format!("{}.{}", minutely.ev, minutely.t);
+                                let msg = Message {
+                                    stream,
+                                    data: serde_json::to_value(minutely)?,
+                                };
+                                client_sink.send(WebsocketMessage::Text(serde_json::to_string(&msg)?)).await?;
+                            }
+                            None => log::warn!("Missing datum number {}", x),
+                        }
+                    }
+                    log::info!("  prerolling finished");
+                }
+                x => {
+                    client_sink = err_to_client(client_sink, format!("Unknown command type {}", x)).await?;
+                }
+            }
+            let ret : anyhow::Result<_> = Ok(client_sink);
+            ret
+        }
+    };
+
+    while let Some(msg) = client_stream.next().await  {
+        match msg {
+            Ok(WebsocketMessage::Text(msg)) => client_sink = handle_msg(serde_json::from_str(&msg)?, client_sink).await?,
+            Ok(WebsocketMessage::Binary(msg)) => client_sink = handle_msg(serde_json::from_slice(&msg)?, client_sink).await?,
+            Ok(WebsocketMessage::Close(c)) => {
+                log::warn!("Close message from upstream: {:?}", c);
+            }
+            Ok(WebsocketMessage::Ping(_)) => {
+                log::debug!("WebSocket ping from upstream");
+            }
+            Ok(_) => {
+                log::warn!("other WebSocket message from upstream");
+            }
+            Err(e) => log::error!("From upstream websocket: {}", e),
+        }
     }
     Ok(())
 }
@@ -54,13 +146,7 @@ async fn handle_upstream(config: &ClientConfig, db: &sled::Db) -> anyhow::Result
     }
     log::debug!("Establishing upstream connection 3");
 
-    let mut nextkey = match db.last()? {
-        None => 0,
-        Some((k, _v)) => {
-            let k : [u8; 8] = k.as_ref().try_into()?;
-            u64::from_be_bytes(k).checked_add(1).context("Key space is full")?
-        }
-    };
+    let mut nextkey = get_next_db_key(&db)?;
 
     let mut handle_msg = |msg: Message| -> anyhow::Result<()> {
         match &msg.stream[..] {
@@ -111,13 +197,15 @@ async fn main() -> anyhow::Result<()> {
 
     let listener = tokio::net::TcpListener::bind(opts.listen_addr).await?;
 
+    let db_ = db.clone();
     tokio::spawn(async move {
         loop {
             match listener.accept().await  {
                 Ok((client_socket, addr)) => {
                     log::info!("Incoming client connection from {}", addr);
+                    let db__ = db_.clone();
                     tokio::spawn(async move {
-                        if let Err(e) = serve_client(client_socket).await {
+                        if let Err(e) = serve_client(client_socket, &db__).await {
                             log::error!("Error serving client: {}", e);
                         }
                         log::info!("Finished serving client from {}", addr);
