5985b50b943148aef593e44dd9e2508416ded658
Vitaly _Vi Shukela <vi0oss@gmail.com>
2021-09-17 01:17:59 +0300

    Refactor, begin filter

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::convert::TryInto;
 use std::{net::SocketAddr, path::PathBuf};
 
-use std::time::Duration;
 use anyhow::Context;
 use futures::stream::SplitSink;
-use futures::{sink::SinkExt,stream::StreamExt};
+use futures::{sink::SinkExt, stream::StreamExt};
+use std::time::Duration;
 
 use tokio_tungstenite::tungstenite::Message as WebsocketMessage;
 
@@ -31,7 +31,7 @@ struct Message {
 struct MinutelyData {
     ev: String,
 
-    #[serde(rename="T")]
+    #[serde(rename = "T")]
     t: String,
 
     #[serde(flatten)]
@@ -50,160 +50,218 @@ fn get_next_db_key(db: &sled::Db) -> anyhow::Result<u64> {
     let nextkey = match db.last()? {
         None => 0,
         Some((k, _v)) => {
-            let k : [u8; 8] = k.as_ref().try_into()?;
-            u64::from_be_bytes(k).checked_add(1).context("Key space is full")?
+            let k: [u8; 8] = k.as_ref().try_into()?;
+            u64::from_be_bytes(k)
+                .checked_add(1)
+                .context("Key space is full")?
         }
     };
     Ok(nextkey)
 }
 
-async fn serve_client(client_socket: tokio::net::TcpStream, db: &sled::Db) -> anyhow::Result<()> {
-    let (mut client_sink, mut client_stream) = tokio_tungstenite::accept_async(client_socket).await?.split();
-    {
-        let buf = serde_json::to_string(&Message{stream:"hello".to_owned(), data: serde_json::Value::Null}).unwrap();
-        client_sink.send(WebsocketMessage::Text(buf)).await?;
+struct ServeClient {
+    ws: tokio_tungstenite::WebSocketStream<tokio::net::TcpStream>,
+    db: sled::Db,
+    cursor: u64,
+    filter: Option<hashbrown::HashSet<smol_str::SmolStr>>,
+}
+
+impl ServeClient {
+    async fn run(mut self) -> anyhow::Result<()> {
+        {
+            let buf = serde_json::to_string(&Message {
+                stream: "hello".to_owned(),
+                data: serde_json::Value::Null,
+            })
+            .unwrap();
+            self.ws.send(WebsocketMessage::Text(buf)).await?;
+        }
+
+        while let Some(msg) = self.ws.next().await {
+            match msg {
+                Ok(WebsocketMessage::Text(msg)) => {
+                    self.handle_msg(serde_json::from_str(&msg)?).await?
+                }
+                Ok(WebsocketMessage::Binary(msg)) => {
+                    self.handle_msg(serde_json::from_slice(&msg)?).await?
+                }
+                Ok(WebsocketMessage::Close(c)) => {
+                    log::info!("Close message from client: {:?}", c);
+                }
+                Ok(WebsocketMessage::Ping(_)) => {
+                    log::debug!("WebSocket ping from client");
+                }
+                Ok(_) => {
+                    log::warn!("other WebSocket message from client");
+                }
+                Err(e) => log::error!("From client websocket: {}", e),
+            }
+        }
+        Ok(())
     }
-    let mut cursor = get_next_db_key(db)?;
- 
-    let err_to_client = |mut client_sink: SplitSink<_,_>, x:String| {
+
+    async fn err(&mut self, x: String) -> anyhow::Result<()> {
         log::warn!("Sending error to client: {}", x);
-        async move {
-            client_sink.send(WebsocketMessage::Text(serde_json::to_string(&Message{stream:"error".to_owned(), data:serde_json::Value::String(x)}).unwrap())).await?;
-            let ret : anyhow::Result<_> = Ok(client_sink);
-            ret
-        }  
-    };
-
-    let datum_to_client = |v: sled::IVec, mut client_sink:  SplitSink<_,_>, id: u64| -> _ {
-        async move {
-            let minutely : MinutelyData = match serde_json::from_slice(&v) {
-                Ok(x) => x,
-                Err(_e) => {
-                    log::warn!("Failed to properly deserialize minutely datum number {}", id);
-                    return Ok(client_sink);
-                }
-            };
-            let stream = format!("{}.{}", minutely.ev, minutely.t);
-            let msg = Message {
-                stream,
-                data: serde_json::to_value(minutely)?,
-            };
-            client_sink.send(WebsocketMessage::Text(serde_json::to_string(&msg)?)).await?;
-            let ret : anyhow::Result<_> = Ok(client_sink);
-            ret 
-        }
-    };
-
-    let preroller = |mut client_sink:  SplitSink<_,_>, range: _| -> _ {
-        async move {
-            for x in range {
-                let x : u64 = x;
-                match db.get(x.to_be_bytes())? {
-                    Some(v) => {
-                        client_sink = datum_to_client(v, client_sink, x).await?;
-                    }
-                    None => log::warn!("Missing datum number {}", x),
-                }
-            }
-            let ret : anyhow::Result<_> = Ok(client_sink);
-            ret 
-        }
-    };
-
-    let handle_msg = |msg: Message, mut client_sink:  SplitSink<_,_>| -> _ {
-        async move {
-            match &msg.stream[..] {
-                "preroll" => {
-                    let num = match msg.data {
-                        serde_json::Value::Number(x) => x.as_u64().context("Cannot handle nubmer of preroll messages")?,
-                        _ => {
-                            client_sink = err_to_client(client_sink, "Invalid type of `data` for `preroll` message, expected a number".to_owned()).await?;
-                            return Ok(client_sink)
-                        }
-                    };
-                    log::info!("  prerolling {} messages for the client", num);
-                    let start = cursor.saturating_sub(num);
-                    client_sink = preroller(client_sink, start..cursor).await?;
-                    {
-                        let buf = serde_json::to_string(&Message{stream:"preroll_finished".to_owned(), data: serde_json::Value::Null}).unwrap();
-                        client_sink.send(WebsocketMessage::Text(buf)).await?;
-                    }
-                    log::debug!("  prerolling finished");
-                }
-                "monitor" => {
-                    log::info!("  streaming messages for the client");
-                    let mut watcher = db.watch_prefix(vec![]);
-                    
-                    // https://github.com/spacejam/sled/issues/1368
-                    while let Some(evt) = (&mut watcher).await {
-                        match evt {
-                            sled::Event::Insert { key, value } => {
-                                let key : Result<[u8; 8], _> = key.as_ref().try_into();
-                                if let Ok(key) = key {
-                                    let key = u64::from_be_bytes(key);
-                                    client_sink = preroller(client_sink, cursor..key).await?;
-                                    client_sink = datum_to_client(value, client_sink, key).await?;
-                                    cursor = key.saturating_add(1);
-                                }
-                            }
-                            sled::Event::Remove { key:_ } => (),
-                        }
-                    }
-                    log::debug!("  streaming messages finished");
-                }
-                "remove_retaining_last_n" => {
-                    let num = match msg.data {
-                        serde_json::Value::Number(x) => x.as_u64().context("Cannot handle nubmer of remove_retaining_last_n messages")?,
-                        _ => {
-                            client_sink = err_to_client(client_sink, "Invalid type of `data` for `remove_retaining_last_n` message, expected a number".to_owned()).await?;
-                            return Ok(client_sink)
-                        }
-                    };
-                    log::info!("  retaining {} last samples in the database", num);
-                    let first = (0u64).to_be_bytes();
-                    let last = cursor.saturating_sub(num).to_be_bytes();
-                    let mut ctr = 0u64;
-                    for x in db.range(first..last) {
-                        if let Ok((key, _val)) = x {
-                            match db.remove(&key) {
-                                Err(e) => log::error!("Error removing entry {:?}: {}", key, e),
-                                Ok(None) => (),
-                                Ok(Some(_)) => ctr += 1,
-                            }
-                        }
-                    }
-                    {
-                        let buf = serde_json::to_string(&Message{stream:"remove_finished".to_owned(), data: serde_json::Value::Number(ctr.into())}).unwrap();
-                        client_sink.send(WebsocketMessage::Text(buf)).await?;
-                    }
-                    log::info!("  finished removing {} entries", ctr);
-                }
-                x => {
-                    client_sink = err_to_client(client_sink, format!("Unknown command type {}", x)).await?;
-                }
-            }
-            let ret : anyhow::Result<_> = Ok(client_sink);
-            ret
-        }
-    };
-
-    while let Some(msg) = client_stream.next().await  {
-        match msg {
-            Ok(WebsocketMessage::Text(msg)) => client_sink = handle_msg(serde_json::from_str(&msg)?, client_sink).await?,
-            Ok(WebsocketMessage::Binary(msg)) => client_sink = handle_msg(serde_json::from_slice(&msg)?, client_sink).await?,
-            Ok(WebsocketMessage::Close(c)) => {
-                log::info!("Close message from client: {:?}", c);
-            }
-            Ok(WebsocketMessage::Ping(_)) => {
-                log::debug!("WebSocket ping from client");
-            }
-            Ok(_) => {
-                log::warn!("other WebSocket message from client");
-            }
-            Err(e) => log::error!("From client websocket: {}", e),
-        }
+        self.ws.send(WebsocketMessage::Text(
+                    serde_json::to_string(&Message {
+                        stream: "error".to_owned(),
+                        data: serde_json::Value::String(x),
+                    })
+                    .unwrap(),
+                ))
+                .await?;
+        Ok(())
     }
-    Ok(())
+
+    async fn datum(&mut self, v: sled::IVec, id: u64) -> anyhow::Result<()> {
+        let minutely: MinutelyData = match serde_json::from_slice(&v) {
+            Ok(x) => x,
+            Err(_e) => {
+                log::warn!(
+                    "Failed to properly deserialize minutely datum number {}",
+                    id
+                );
+                return Ok(());
+            }
+        };
+        if let Some(filt) = &self.filter {
+            if ! filt.contains(&smol_str::SmolStr::new(&minutely.t)) {
+                return Ok(());
+            }
+        }
+        let stream = format!("{}.{}", minutely.ev, minutely.t);
+        let msg = Message {
+            stream,
+            data: serde_json::to_value(minutely)?,
+        };
+        self.ws
+            .send(WebsocketMessage::Text(serde_json::to_string(&msg)?))
+            .await?;
+        Ok(())
+    }
+
+    async fn preroller(&mut self, range: impl Iterator<Item=u64>) -> anyhow::Result<()> {
+        for x in range {
+            let x: u64 = x;
+            match self.db.get(x.to_be_bytes())? {
+                Some(v) => {
+                    self.datum(v, x).await?;
+                }
+                None => log::warn!("Missing datum number {}", x),
+            }
+        }
+        Ok(())
+    }
+
+    async fn handle_msg(&mut self, msg: Message) -> anyhow::Result<()> {
+        match &msg.stream[..] {
+            "preroll" => {
+                let num = match msg.data {
+                    serde_json::Value::Number(x) => x
+                        .as_u64()
+                        .context("Cannot handle nubmer of preroll messages")?,
+                    _ => {
+                        self.err(
+                            "Invalid type of `data` for `preroll` message, expected a number"
+                                .to_owned(),
+                        )
+                        .await?;
+                        return Ok(());
+                    }
+                };
+                log::info!("  prerolling {} messages for the client", num);
+                let start = self.cursor.saturating_sub(num);
+                self.preroller(start..self.cursor).await?;
+                {
+                    let buf = serde_json::to_string(&Message {
+                        stream: "preroll_finished".to_owned(),
+                        data: serde_json::Value::Null,
+                    })
+                    .unwrap();
+                    self.ws.send(WebsocketMessage::Text(buf)).await?;
+                }
+                log::debug!("  prerolling finished");
+            }
+            "monitor" => {
+                log::info!("  streaming messages for the client");
+                let mut watcher = self.db.watch_prefix(vec![]);
+
+                // https://github.com/spacejam/sled/issues/1368
+                while let Some(evt) = (&mut watcher).await {
+                    match evt {
+                        sled::Event::Insert { key, value } => {
+                            let key: Result<[u8; 8], _> = key.as_ref().try_into();
+                            if let Ok(key) = key {
+                                let key = u64::from_be_bytes(key);
+                                self.preroller(self.cursor..key).await?;
+                                self.datum(value, key).await?;
+                                self.cursor = key.saturating_add(1);
+                            }
+                        }
+                        sled::Event::Remove { key: _ } => (),
+                    }
+                }
+                log::debug!("  streaming messages finished");
+            }
+            "remove_retaining_last_n" => {
+                let num = match msg.data {
+                    serde_json::Value::Number(x) => x
+                        .as_u64()
+                        .context("Cannot handle nubmer of remove_retaining_last_n messages")?,
+                    _ => {
+                        self.err("Invalid type of `data` for `remove_retaining_last_n` message, expected a number".to_owned()).await?;
+                        return Ok(());
+                    }
+                };
+                log::info!("  retaining {} last samples in the database", num);
+                let first = (0u64).to_be_bytes();
+                let last = self.cursor.saturating_sub(num).to_be_bytes();
+                let mut ctr = 0u64;
+                for x in self.db.range(first..last) {
+                    if let Ok((key, _val)) = x {
+                        match self.db.remove(&key) {
+                            Err(e) => log::error!("Error removing entry {:?}: {}", key, e),
+                            Ok(None) => (),
+                            Ok(Some(_)) => ctr += 1,
+                        }
+                    }
+                }
+                {
+                    let buf = serde_json::to_string(&Message {
+                        stream: "remove_finished".to_owned(),
+                        data: serde_json::Value::Number(ctr.into()),
+                    })
+                    .unwrap();
+                    self.ws.send(WebsocketMessage::Text(buf)).await?;
+                }
+                log::info!("  finished removing {} entries", ctr);
+            }
+            "database_size" => {
+                let buf = serde_json::to_string(&Message {
+                    stream: "database_size".to_owned(),
+                    data: serde_json::Value::Number(self.db.size_on_disk()?.into()),
+                })
+                .unwrap();
+                self.ws.send(WebsocketMessage::Text(buf)).await?;
+            }
+            x => {
+                self.err(format!("Unknown command type {}", x)).await?;
+            }
+        }
+        Ok(())
+    }
+}
+
+async fn serve_client(client_socket: tokio::net::TcpStream, db: &sled::Db) -> anyhow::Result<()> {
+    let ws  = tokio_tungstenite::accept_async(client_socket).await?;
+    let cursor = get_next_db_key(db)?;
+    let c = ServeClient {
+        ws,
+        db: db.clone(),
+        cursor,
+        filter: None,
+    };
+    c.run().await
 }
 
 async fn handle_upstream(config: &ClientConfig, db: &sled::Db) -> anyhow::Result<()> {
@@ -211,7 +269,9 @@ async fn handle_upstream(config: &ClientConfig, db: &sled::Db) -> anyhow::Result
     let (mut upstream, _) = tokio_tungstenite::connect_async(&config.uri).await?;
     log::debug!("Establishing upstream connection 2");
     for startup_msg in &config.startup_messages {
-        upstream.send(WebsocketMessage::Text(serde_json::to_string(startup_msg)?)).await?;
+        upstream
+            .send(WebsocketMessage::Text(serde_json::to_string(startup_msg)?))
+            .await?;
     }
     log::debug!("Establishing upstream connection 3");
 
@@ -237,7 +297,7 @@ async fn handle_upstream(config: &ClientConfig, db: &sled::Db) -> anyhow::Result
         Ok(())
     };
 
-    while let Some(msg) = upstream.next().await  {
+    while let Some(msg) = upstream.next().await {
         match msg {
             Ok(WebsocketMessage::Text(msg)) => handle_msg(serde_json::from_str(&msg)?)?,
             Ok(WebsocketMessage::Binary(msg)) => handle_msg(serde_json::from_slice(&msg)?)?,
@@ -288,7 +348,7 @@ async fn main() -> anyhow::Result<()> {
     let db_ = db.clone();
     tokio::spawn(async move {
         loop {
-            match listener.accept().await  {
+            match listener.accept().await {
                 Ok((client_socket, addr)) => {
                     log::info!("Incoming client connection from {}", addr);
                     let db__ = db_.clone();
@@ -298,7 +358,7 @@ async fn main() -> anyhow::Result<()> {
                         }
                         log::info!("Finished serving client from {}", addr);
                     });
-                },
+                }
                 Err(e) => {
                     log::error!("Listening incoming connections: {}", e);
                 }
